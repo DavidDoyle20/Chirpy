6dbf8de219a0ae855b9acc2e9e2b4c8e41f48c86
DavidDoyle20 <davestar80@gmail.com>
2024-12-04 12:07:11 -0600

    added a basic html file

--- main.go
+++ main.go
@@ -6,11 +6,16 @@ import (
 )
 
 func main() {
+	// multiplexer: connects one input line to an output line
 	mux := http.NewServeMux()
-	server := http.Server {
-		Addr: "localhost:8080",
+	server := &http.Server{
+		Addr:    "localhost:8080",
 		Handler: mux,
 	}
+
+	// connects a pattern in the url to a file path
+	mux.Handle("/", http.FileServer(http.Dir(".")))
+
 	err := server.ListenAndServe()
 	if err != nil {
 		fmt.Println(err)
